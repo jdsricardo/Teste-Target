aa7adb958cda44cf58778cf9c978c62ba3fc1281
Ricardo José da Silva <128723474+jdsricardo@users.noreply.github.com>
2024-04-22 21:10:39 -0300

    Add files via upload

---
+++ Teste Target/fibonacci.py
@@ -0,0 +1,16 @@
+print('-'*30)
+print('Sequência de Fibonacci')
+print('-'*30)
+
+n = int(input('Quantos termos você quer mostrar?'))
+termo1 = 0
+termo2 = 1
+contador = 3
+print('{} -> {}'.format(termo1,termo2),end='')
+while contador <= n:
+    termo3 = termo1 + termo2
+    print(' -> {}'.format(termo3),end='')
+    termo1 = termo2
+    termo2 = termo3
+    contador+=1
+print(' -> FIM')
---
+++ Teste Target/inverte.py
@@ -0,0 +1,3 @@
+string = str(input('Escreva a palavra a ser invertida: '))
+string_invertida = string[::-1]
+print(string_invertida)
